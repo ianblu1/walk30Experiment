bacbc9f49db375a811ba45248affedcc27b2b9f5
ianblu1 <ianblu1@gmail.com>
2012-10-25 22:00:17 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Walk30Experiment::Application.config.secret_token = 'a561d10d692ca38e9b06fd91dfec2b3255baa486607654627bc93ab8aa595ca14a58b6016852ab546e2a31836b86077bbc4a9890223ac65bb48c95f493bf25d1'
