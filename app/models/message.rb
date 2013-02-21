118067c223a16e76c27fd2e5b2756c9b4981c975
Amory Schlender <amory.schlender@gmail.com>
2013-02-20 22:08:07 -0800

    Fixes Message.replies method so that autoreplies, which can be very fast, are processed correctly.

--- app/models/message.rb
+++ app/models/message.rb
@@ -69,15 +69,16 @@ class Message < ActiveRecord::Base
   
   def replies
     if self.sent_at
-      ms = self.participant.messages.select {|m| m.sent_at and m.sent_at> self.sent_at}
       if self.delivered?
+        ms = self.participant.messages.select {|m| m.sent_at and m.sent_at > self.sent_at}
         upper_time = ms.select{|m| m.delivered? and m.sent_at}.map{|m| m.sent_at}.min
         upper_time = Time.now if not upper_time
-        ms.select {|m| m.sent_at and m.sent_at < upper_time and m.received?}
+        ms.select {|m| m.sent_at and m.sent_at <= upper_time and m.received?}
       elsif self.received?
+        ms = self.participant.messages.select {|m| m.sent_at and m.sent_at >= self.sent_at and m.id != self.id}
         upper_time = ms.select{|m| m.received? and m.sent_at}.map{|m| m.sent_at}.min
         upper_time = Time.now if not upper_time
-        ms.select {|m| m.sent_at and m.sent_at < upper_time and m.delivered?}
+        ms.select {|m| m.sent_at and m.sent_at <= upper_time and m.delivered?}
       end
     end
   end
